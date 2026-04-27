fc2d186d44f8c46bcdecb9282d8b464e3bf428f3
Anitha <anitha.gorli@quantela.com>
2026-04-27 12:44:25 +0530

    Fix- Namankit Registration

--- mhdbt-school-registration-namankit/src/index.js
+++ mhdbt-school-registration-namankit/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';  // ✅ This should work if App.tsx exists
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap-icons/font/bootstrap-icons.css';
 import './styles/App.css';
 
 import { createRoot } from "react-dom/client";
