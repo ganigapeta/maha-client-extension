80de8e00580b675a467ff787ba773696cd14f384
Anitha <anitha.gorli@quantela.com>
2026-05-08 13:36:48 +0530

    fix - FPS code also showing along with Name

--- apl_scheme_scrutiny/src/pages/SchemeSearch.js
+++ apl_scheme_scrutiny/src/pages/SchemeSearch.js
@@ -184,7 +184,7 @@ useEffect(() => {
   };
 
   const isFormValid = () => {
-    const baseValid = formData.financialYear && formData.month ;//&& formData.fpsName;
+    const baseValid = formData.financialYear && formData.month ;//&& formData.fpsCode;
     // For DFSO role, AFSO office is also mandatory
     if (userRole === 'DFSO') {
       return baseValid && formData.afsoCode && formData.fpsCode;
